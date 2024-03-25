77f56a2de18515c4871d7f5d5708b3d7362e2ccd
Pakinun <pakinun@i3gateway.com>
2024-03-25 23:20:12 +0700

    install lib redyx, vector-icons, formik, yup

--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -1,12 +1,20 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 
 const Login = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Login</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Login;
